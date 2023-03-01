d83931060ecb12b20b18dd97bb919fedd82c550e
nfgrep <nfg2600@gmail.com>
2023-03-01 01:38:08 -0500

    Flesh out program

--- lib/tldr.rb
+++ lib/tldr.rb
@@ -1,25 +1,52 @@
 require 'tmpdir'
 require 'rtesseract'
-require 'dotenv/load'
 require 'openai'
 
 module Tldr
-  def initialize
-    client = OpenAI::Client.new(access_token: ENV['OPENAI_ACCESS_TOKEN'])
+  class << self
+    def new
+      api_key = ENV['OPENAI_ACCESS_TOKEN'] || load_api_key()
+      unless api_key
+        puts "Please provide your OpenAI API key:"
+        api_key = gets.chomp
+        save_api_key(api_key)
+      end
 
-    Dir.mktmpdir do |dir|
-      img_fpath = File.join(dir, 'screenshot')
-      `screencapture -i #{img_fpath}`
-      captured_string = RTesseract.new(img_fpath).to_s
-      response = client.completions(
-        parameters: {
-          model: "text-davinci-003",
-          prompt: "#{captured_string}. In summary: ",
-          max_tokens: 256
-        }
-      )
+      client = OpenAI::Client.new(access_token: api_key)
 
-      puts response["choices"].map { |c| c["text"] }
+      Dir.mktmpdir do |dir|
+        img_fpath = File.join(dir, 'screenshot')
+        `screencapture -i #{img_fpath}`
+        captured_string = RTesseract.new(img_fpath).to_s
+        response = client.completions(
+          parameters: {
+            model: "text-davinci-003",
+            prompt: "#{captured_string}. In summary: ",
+            max_tokens: 256
+          }
+        )
+
+        puts response["choices"].map { |c| c["text"] }
+      end
+    end
+
+    def save_api_key(api_key)
+      # Save the API key to in a file under ~/.config/tldr-screenshot/api_key
+      # Create the directory if it doesn't exist
+      config_dir = File.join(Dir.home, '.config', 'tldr-screenshot')
+      FileUtils.mkdir_p(config_dir) unless Dir.exist?(config_dir)
+
+      api_key_fpath = File.join(config_dir, 'api_key')
+      puts "Saving API key to #{config_dir}"
+      File.write(api_key_fpath, api_key)
+    end
+
+    def load_api_key
+      # Load the API key from ~/.config/tldr-screenshot/api_key
+      config_dir = File.join(Dir.home, '.config', 'tldr-screenshot')
+      return nil unless Dir.exist?(config_dir)
+      api_key_fpath = File.join(config_dir, 'api_key')
+      File.read(api_key_fpath)
     end
   end
 end
--- tldr-screenshot.gemspec
+++ tldr-screenshot.gemspec
@@ -15,7 +15,7 @@ Gem::Specification.new do |spec|
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+    # spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
     spec.metadata["homepage_uri"] = spec.homepage
     spec.metadata["source_code_uri"] = "https://github.com/nfgrep/tldr-screenshot"
@@ -40,4 +40,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rtesseract"
   spec.add_development_dependency "dotenv"
   spec.add_development_dependency "ruby-openai"
+
+  spec.add_runtime_dependency "rtesseract"
+  spec.add_runtime_dependency "ruby-openai"
 end
